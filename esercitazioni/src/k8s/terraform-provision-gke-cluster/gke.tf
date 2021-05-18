e57cd0994ab2f5a5d60d0c64d548a796383de89a
U0I4229 <edoardo.pelli@nttdata.com>
2021-05-18 19:07:59 +0200

    deleted duplicated variables from gke file

--- esercitazioni/src/k8s/terraform-provision-gke-cluster/gke.tf
+++ esercitazioni/src/k8s/terraform-provision-gke-cluster/gke.tf
@@ -1,17 +1,4 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
 
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
-variable "gke_num_nodes" {
-  default     = 1
-  description = "number of gke nodes"
-}
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
